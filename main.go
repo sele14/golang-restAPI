7909ec40d951fe751dca814e6e015cd9269c3073
sele14 <eirik.selander@gmail.com>
2021-02-04 23:21:37 +0000

    input edit

--- main.go
+++ main.go
@@ -5,61 +5,59 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
 	"github.com/gorilla/mux"
 )
 
 // GOAL:
-// an API that stores the ingredients for beer--e.g. an inventory for a beer factory.
-// Allows for post, get, and delete requests of beer ingredients.
+// an API that stores your investment portfolio.
+// Allows for post, get, and delete requests of assets.
 
 // (Note: This is file is more highly commented than usual for my own benefit.)
 // ___________________________
 
-
-
-// Create the different fields we expect for our beer ingredients (struct)
+// Create the different fields we expect for our investments (struct)
 // The struct is our 'model' of our data (similar to a class)
-
-type Ingredient struct {
-	ID string `json:"id"`
-	Name string `json:"name"`
-	Desc string `json:"desc"`
-	Quantity int `"json:"quantity"`
+type Instrument struct {
+	ID       string  `json:"id"`
+	Type     string  `json:"type"`
+	Name     string  `json:"name"`
+	Price    float32 `json:"price"`
+	Quantity int     `json:"quantity"`
 }
 
-// declare the ingredients var
+// declare the instruments var
+var instruments []Instrument
 
-var ingredients []Ingredient
-
-func root(w http.ResponseWriter, r *http.Request){
+func root(w http.ResponseWriter, r *http.Request) {
 	// Fprintf prints the string to the writer object, not to console (it will show in the browser)
 	fmt.Fprintf(w, "Hello, you are at: root()")
 }
 
-func getIngredients(w http.ResponseWriter, r *http.Request){
+func getInstruments(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	// Println prints to the console, not browser
-	fmt.Println("Function Called: getIngredients()")
+	fmt.Println("Function Called: getInstruments()")
 
-	// list ingredients to the write stream (in the browser)
-	json.NewEncoder(w).Encode(ingredients)
+	// list instruments to the write stream (in the browser)
+	json.NewEncoder(w).Encode(instruments)
 }
 
-func addIngredients(w http.ResponseWriter, r *http.Request){
+func addInstruments(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	
-	var ingredient Ingredient
+
+	var instrument Instrument
 
 	// convert inputted data to object we can work with
-	json.NewDecoder(r.Body).Decode(&ingredient)
-	
+	json.NewDecoder(r.Body).Decode(&instrument)
+
 	// Store the data we add
-	ingredients = append(ingredients, ingredient)
+	instruments = append(instruments, instrument)
 	// Encode the data and return it back, so user can verify stored data
-	json.NewEncoder(w).Encode(ingredient)
+	json.NewEncoder(w).Encode(instrument)
 }
 
-func deleteIngredients(w http.ResponseWriter, r *http.Request){
+func deleteInstruments(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	// Get params from read stream
@@ -68,63 +66,60 @@ func deleteIngredients(w http.ResponseWriter, r *http.Request){
 	// pass the ID into the delete function
 	deleteByID(params["id"])
 
-	json.NewEncoder(w).Encode(ingredients)
+	json.NewEncoder(w).Encode(instruments)
 }
 
-func deleteByID(id string){
-	// iterate by ingredient list
-	for index, ingredient := range ingredients {
+func deleteByID(id string) {
+	// iterate by instruments list
+	for index, instrument := range instruments {
 		// if ID matches; delete
-		if ingredient.ID == id {
-			ingredients = append(ingredients[:index], ingredients[index+1:]...)
+		if instrument.ID == id {
+			instruments = append(instruments[:index], instruments[index+1:]...)
 			break
 		}
 
 	}
 }
 
-func makeRequests(){
-	// create router with short var (auto type, dont have to declare type) 
+func makeRequests() {
+	// create router with short var (auto type, dont have to declare type)
 	router := mux.NewRouter().StrictSlash(true)
 	// Create a GET request
 	router.HandleFunc("/", root).Methods("GET")
 
 	// create get/post/delete requests to interact with the API
-	router.HandleFunc("/ingredients", getIngredients).Methods("GET")
-	router.HandleFunc("/ingredients", addIngredients).Methods("POST")
-	router.HandleFunc("/ingredients/{id}", deleteIngredients).Methods("DELETE")
+	router.HandleFunc("/instruments", getInstruments).Methods("GET")
+	router.HandleFunc("/instruments", addInstruments).Methods("POST")
+	router.HandleFunc("/instruments/{id}", deleteInstruments).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
 
-func main(){
-	// add water as ingredient
-	ingredients = append(ingredients, Ingredient{
-		ID : "0",
-		Name : "Water",
-		Desc : "Water is the largest volume ingredient in beer, and has a significant impact on the end product.",
-		Quantity : 50,
+// Some example asset classes:
+// Equities
+// Fixed Income
+// Cash & Equiv
+// Real Estate
+// Commodities
+// Derivatives
+// Cryptocurrencies
+
+func main() {
+	// add our first instrument
+	instruments = append(instruments, Instrument{
+		ID:       "0",
+		Type:     "Stock",
+		Name:     "ATVI",
+		Price:    92.68,
+		Quantity: 5,
 	})
-	// add another ingredient
-	ingredients = append(ingredients, Ingredient{
-		ID : "1",
-		Name : "Hops",
-		Desc : "Hops are responsible for producing aromas, some flavors and bitterness.",
-		Quantity : 20,
+	// add another instrument
+	instruments = append(instruments, Instrument{
+		ID:       "1",
+		Type:     "Bond",
+		Name:     "TNX",
+		Price:    11.39,
+		Quantity: 3,
 	})
-	// some more sample ingredients--can be added with Postman calls
-	// ingredients = append(ingredients, Ingredient{
-	// 	ID : "2",
-	// 	Name : "Barley",
-	// 	Desc : "Many brewers see barley as beer's soul. Barley has distinctive characteristics that make it a favored ingredient over other grains.",
-	// 	Quantity : 20,
-	// })
-	// // add another ingredient
-	// ingredients = append(ingredients, Ingredient{
-	// 	ID : "3",
-	// 	Name : "Yeast",
-	// 	Desc : "Before it begins to reproduce and provide the beer with alcohol, yeast requires sugar to digest and oxygen to breathe.",
-	// 	Quantity : 10,
-	// })
 	makeRequests()
 }
--- readme.md
+++ readme.md
@@ -17,11 +17,12 @@ go run main.go
 
 Use an API testing tool like Postman or https://reqbin.com/ and send a POST request like adding another beer ingredient:
 
-``
-{
-  	"UID" : "2",
-	"Name" : "Barley",
-	"Desc" : "Many brewers see barley as beer's soul. Barley has distinctive characteristics that make it a favored ingredient over other grains.",
-	"Quantity" : 20
-}
-``
+##  Input:
+
+```go
+	ID       string  `json:"id"`
+	Type     string  `json:"type"`
+	Name     string  `json:"name"`
+	Price    float32 `json:"price"`
+	Quantity int     `json:"quantity"`
+```
